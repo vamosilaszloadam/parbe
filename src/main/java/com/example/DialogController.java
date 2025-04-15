a8618b6ff22596c32a2cd4f365a8ed63ab6cb4ee
Vámosi László Ádám <vamosilaszloadam@outlook.com>
2025-04-15 11:08:19 +0200

    Törlés gomb ok

--- src/main/java/com/example/DialogController.java
+++ src/main/java/com/example/DialogController.java
@@ -2,8 +2,12 @@ package com.example;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+import javafx.scene.control.Alert.AlertType;
+// import javafx.stage.Stage;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class DialogController {
 
@@ -14,7 +18,9 @@ public class DialogController {
 
     @FXML
     void onClickCloseButton(ActionEvent event) {
-        
+        startClose();
+    }
+    private void startClose() {
         // Stage stage = 
         // ((Stage) 
         // ((javafx.scene.Node) event.getSource())
@@ -22,13 +28,31 @@ public class DialogController {
         // .getWindow());
 
         // stage.close();
+        if(plateField.getText().isEmpty()) {
+            System.out.println("üres");
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setHeaderText("Üres");
+            alert.setTitle("Hiba");
+            alert.setContentText("A rendszám nem lehet üres!");
+            alert.showAndWait();
+            return;
+        }
 
         mainController.carList.getItems().add(plateField.getText());
         mainController.stage.close();
-
     }
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
+    }
+
+    @FXML
+    void onKeyReleasedPlateField(KeyEvent event) {
+        // System.out.println(event.getCode());
+        // if (event.getCode().toString() == "ENTER") {
+        if (event.getCode() == KeyCode.ENTER) {
+            startClose();
+        }
+
     }
 
 }
--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -6,7 +6,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.TextFieldListCell;
 import javafx.stage.Stage;
 
 public class MainController {
@@ -19,6 +22,8 @@ public class MainController {
     @FXML
     void initialize() {
         System.out.println("initialize...");
+        carList.setEditable(true);
+        carList.setCellFactory(TextFieldListCell.forListView());
         App._stage.setOnCloseRequest(event -> {
             System.out.println("onCloseRequest...");
 
@@ -54,5 +59,35 @@ public class MainController {
 
         DialogController controller = loader.getController();
         controller.setMainController(this);
+    }
+
+    @FXML
+    void onClickDelButton(ActionEvent event) {
+        System.out.println("törlés...");
+        int index = this.carList
+            .getSelectionModel()
+            .getSelectedIndex();
+        if(index == -1) {
+            System.err.println("Hiba! Ki kell jelölni valamit!");
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Hiba");
+            alert.setHeaderText("Kiválasztás");
+            alert.setContentText("Hiba! Nincs kiválasztva rendszám!");
+            alert.showAndWait();
+            return;
+        }
+        String res = this.carList.getItems().remove(index);
+        System.out.println(index);
+        System.out.println("Törölve: " + res);
+    }
+
+    @FXML
+    void onClickExitButton(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    void onClickModifyButton(ActionEvent event) {
+
     }
 }
